99f277e7392a46887b6858c3f968e55de893e338
ZeyxRew <richard_hofmann@gmx.de>
2022-11-18 10:30:00 +0100

    ssl socket

--- src/fsu/hofmann_grumbach/emailclient/ReadSocket.java
+++ src/fsu/hofmann_grumbach/emailclient/ReadSocket.java
@@ -9,13 +9,17 @@ import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.stream.Stream;
 
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
+
 public class ReadSocket {
 
 	public void start() {
 		String serverAdress = "";
 		int serverPort;
 
-		Socket client = null;
+		//Socket client = null;
+		SSLSocket client= null;
 		System.out.println("Read mails using sockets.");
 		try {
 			// read user input using BufferedReader
@@ -37,9 +41,19 @@ public class ReadSocket {
 				System.out.println("Error: Please provide a server adress");
 				return;
 			}
+<<<<<<< Updated upstream
 
 			// initiate new client with specified adress and port
 			client = new Socket(serverAdress, serverPort);
+=======
+			
+			//initiate new client with specified adress and port
+			//client = new Socket(serverAdress, serverPort);
+			 SSLSocketFactory factory =
+		                (SSLSocketFactory)SSLSocketFactory.getDefault();
+		             client=
+		                (SSLSocket)factory.createSocket(serverAdress, serverPort);
+>>>>>>> Stashed changes
 
 			// define input- and output stream for client-server connection
 			InputStream is = client.getInputStream();
